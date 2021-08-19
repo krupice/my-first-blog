60a507d06478c1ff3bc0631ae169a4834a84252b
Radka Krupkova <krupkova@actia.cz>
2021-08-19 08:38:55 +0200

    My Django Girls app, first commit

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+     url(r'^$', views.post_list, name='post_list'),
+     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+     url(r'^post/new/$', views.post_new, name='post_new'),
+     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+]
